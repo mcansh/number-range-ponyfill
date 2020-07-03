f40da6b4611c5a59d970af849ed197482f32e96a
Logan McAnsh <logan@mcan.sh>
2020-07-03 10:57:15 -0400

    feat: convert to class
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,42 @@
-function range(
-  start: number,
-  end: number = start,
-  step = 1,
-  offset = 0
-): number[] | undefined {
-  if (isNaN(start) || isNaN(end)) return undefined;
-  // if only passed a single arg, rerun function with it as the 'end'
-  if (arguments.length === 1) return range(0, start);
+class Range {
+  public constructor(start: number, end?: number, step = 1, offset = 0) {
+    this.start = start;
+    this.end = end;
+    this.step = step;
+    this.offset = offset;
+    this.range = this.makeRange();
+  }
 
-  // calculate the length of the desired array
-  const length = (Math.abs(end - start) + offset * 2) / step + 1;
-  // determine if we need to count up or down
-  const direction = start < end ? 1 : -1;
-  const startingPoint = start - direction * offset;
-  const stepSize = direction * step;
+  private start: number;
+  private end: number | undefined;
+  private step: number;
+  private offset: number;
+  public range: number[];
 
-  return Array(length)
-    .fill(null)
-    .map((_, index) => startingPoint + stepSize * index);
+  public take(count: number) {
+    return this.range.slice(0, count);
+  }
+
+  private makeRange() {
+    // if only passed a single arg, rerun function with it as the 'end'
+    if (!this.end) {
+      this.end = this.start;
+      this.start = 0;
+    }
+    // return early if start or end are not numbers
+    if (isNaN(this.start) || isNaN(this.end)) return [];
+    // calculate the length of the desired array
+    const length =
+      (Math.abs(this.end - this.start) + this.offset * 2) / this.step + 1;
+    // determine if we need to count up or down
+    const direction = this.start < this.end ? 1 : -1;
+    const startingPoint = this.start - direction * this.offset;
+    const stepSize = direction * this.step;
+
+    return Array(length)
+      .fill(null)
+      .map((_, index) => startingPoint + stepSize * index);
+  }
 }
 
-const take = (array: any[], count: number) => array.slice(0, count);
-
-export { range, take };
+export { Range };
